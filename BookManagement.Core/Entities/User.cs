9e923a206882fcce6067a54753f7f11722a1bd63
GabrielPradoDev <gabrielrpcoelho237@gmail.com>
2024-12-02 22:08:04 -0300

    Ajustes pontuais nas entidades

--- BookManagement.Core/Entities/User.cs
+++ BookManagement.Core/Entities/User.cs
@@ -1,22 +1,21 @@
-﻿namespace BookManagement.Core.Entities
+﻿namespace BookManagement.Core.Entities;
+
+public class User
 {
-    public class User
+    public User(int id,string? name, string? email, string? phone)
     {
-        public User(int id,string? name, string? email, string? phone)
-        {
-            Id = id;
-            Name = name;
-            Email = email;
-            Phone = phone;
-        }
-        public User()
-        {
-
-        }
-
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
+        Id = id;
+        Name = name;
+        Email = email;
+        Phone = phone;
     }
+    public User()
+    {
+
+    }
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
 }
